2b4382c97943df6a07de3c6d4615b52213b05b24
Iyarr <8.24ridiculous@gmail.com>
2025-01-22 11:59:58 +0900

    #1 デバック環境の構築エラー

--- webserver/src/main.rs
+++ webserver/src/main.rs
@@ -17,18 +17,28 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
+    let get = b"GET / HTTP/1.1\r\n";
+
+    let mut contents = String::new();
 
     stream.read(&mut buffer).unwrap();
 
-    let mut file = File::open("index.html").unwrap();
+    if buffer.starts_with(get) {
+        let mut file = File::open("index.html").unwrap();
 
-    let mut contents = String::new();
-    file.read_to_string(&mut contents).unwrap();
+        file.read_to_string(&mut contents).unwrap();
 
-    let response = format!("HTTP/1.1 200 OK\r\n\r\n{}",contents);
+        let response = format!("HTTP/1.1 200 OK\r\n\r\n{}",contents);
 
-    stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
+        stream.write(response.as_bytes()).unwrap();
+    } else {
+        let contents = String::from("のっとふぁうんど");
 
+        let response = format!("HTTP/1.1 400 Not Found\r\n\r\n{}",contents);
+
+        stream.write(response.as_bytes()).unwrap();
+    }
+
+    stream.flush().unwrap(); 
     println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 }
