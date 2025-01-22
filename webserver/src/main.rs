1181aa72983d2e76bb4d897950ccd03dcf7aef0b
Iyarr <8.24ridiculous@gmail.com>
2025-01-22 13:43:56 +0900

    シングルスレッドのWebサーバーの構築

--- webserver/src/main.rs
+++ webserver/src/main.rs
@@ -2,6 +2,8 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
+use std::thread;
+use std::time::Duration;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -18,24 +20,26 @@ fn main() {
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
     let get = b"GET / HTTP/1.1\r\n";
-
-    let mut contents = String::new();
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
 
     stream.read(&mut buffer).unwrap();
-
-    if buffer.starts_with(get) {
-        let mut file = File::open("index.html").unwrap();
-        file.read_to_string(&mut contents).unwrap();
-        let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
-
-        stream.write(response.as_bytes()).unwrap();
-    } else {
-        let contents = String::from("<html><head><meta charset='UTF-8'/></head><body><H1>のっとふぁうんど</H1></body></html>");
-        let response = format!("HTTP/1.1 400 Not Found\r\n\r\n{}", contents);
-
-        stream.write(response.as_bytes()).unwrap();
+    let (first_line,filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n","index.html")
+    } 
+    else if buffer.starts_with(sleep){
+        thread::sleep(Duration::from_secs(5));
+        ("HTTP/1.1 200 OK\r\n\r\n","index.html")
     }
+    else {
+        ("HTTP/1.1 400 Not Found\r\n\r\n","404.html")
+    };
 
+    let mut contents = String::new();
+    let mut file = File::open(filename).unwrap();
+    file.read_to_string(&mut contents).unwrap();
+    let response = format!("{}{}", first_line,contents);
+
+    stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
     println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 }
